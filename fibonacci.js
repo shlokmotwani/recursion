373e8b7c64ab433c383a7392d68a47e0a366ab86
Shlok Motwani <shlok.codes@gmail.com>
2024-06-21 13:44:09 +0530

    fibsRec

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,5 @@
+// fibsRec(10) = [0, 1,  1,  2,  3, 5, 8, 13, 21, 34]
+
 function fibs(num) {
   let arr = [0, 1];
 
@@ -6,3 +8,18 @@ function fibs(num) {
   }
   return arr.slice(0, num);
 }
+
+function fibsRec(num, arr = [0, 1]){
+  if (arr.length >= num) {
+    return arr.slice(0, num);
+  }
+
+  arr.push(arr.at(-1) + arr.at(-2));
+  return fibsRec(num, arr);
+};
+
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(10));
+console.log(fibsRec(6500));
+
